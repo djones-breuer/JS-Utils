db24fde6a267c2fe1851455237d0cfa1e22bf747
David Jones <djones@breuer.com>
2011-04-04 10:06:13 -0400

    fixed a number of issues

--- jquery.sort.js
+++ jquery.sort.js
@@ -1,51 +1,69 @@
 (function( $ ){
   $.fn.sortable = function(settings) {
   
+	var scrollParent; 
+	var sortableObject = $(this); 
+	var offsetTop = $(this).offset().top; 
+
+	var sortComplete = function() {
+		$$.trigger("sort:complete");
+	}; 
+	
 	var config = {
 		scroll			   : true,
-		sortElement		   : "li",
+		sortElement		   : 'li',
 		handle			   : '.handle',
 		scrollSensitivity  : 40, 
 		scrollSpeed		   : 20, 
-		scrollParentClass  : '.scrolling-el'
+		scrollParentClass  : '.scrolling-el', 
+		
+		//callbacks
+		moveComplete	   : sortComplete
+		
 	};
-	
-	var scrolled = 0; 
-	var scrollParent; 
-	
 
 	if (settings) {
 		$.extend(config, settings);
 		
 	}
 	
+	//Requires sortable object to have a unique id; add one if not present- made unique by referencing object y position 
+	if(!sortableObject.attr("id")) {
+		objId = "sortable"+offsetTop;
+		objId = objId.split(".")[0];
+		sortableObject.attr("id", objId);
+	}
+	
+	//build drop zone limiter for proper scoping
+	var sortScope = "#"+sortableObject.attr("id")+" > "+config.sortElement;
+	
+	//since we're using an absolutely position proxy during dragging, we'll need the parent to have a declared position
 	sortElement = $(this).find(config.sortElement);
-	if ($(this).css("position") !="absolute" && $(this).css("position") != "fixed") {
-		$(this).css("position", "relative")
+	if ($(this).css('position') !='absolute' && $(this).css('position') != 'fixed') {
+		$(this).css('position', 'relative')
     }
 
 		sortElement
 		
-				.drag("dragstart", function(ev, dd) {
-	
+				.drag('dragstart', function(ev, dd) {
+					
+					sortableObject.addClass('sorting');
+					
+					//requires handle 
 					if (!$(ev.target).is(config.handle)) { return false; }
+					
+					//locate and name nested scroll-y parent in the dom, if present
 					scrollParent = $(this).parents().filter(config.scrollParentClass)[0]; 
-						
-					if (config.scroll && scrollParent) { scrolled = scrollParent.scrollTop; }
 					
 					//copy the dragged item so you'll see it as you drag. Make it semi-translucent 
-					$(this).clone().addClass("active").css("opacity", .8).insertAfter(this);
+					$(this).clone().addClass('active').css({opacity: .8, position: "absolute"}).insertAfter(this);
 					//hide the currently dragged item
-					$(this).css("opacity", 0);
-					console.log(dd);
-					
+					$(this).css('opacity', .2);
+				
 				})
 				.drag(function( ev, dd ){
-			
-					// in tests, including halfHandle in the eq made up for containers w/ margins 
-					var halfHandle = $(this).find(".handle").outerHeight(true) / 2 ; 
 					
-					//scrolling
+					//used for scrolling only (supports only vertical scrolling)
 					if (config.scroll) {	
 						if (scrollParent) {
 							//scroll parent container that has y-overflow: scroll; lazy recogniztion of this container via class
@@ -62,35 +80,39 @@
 								$(document).scrollTop($(document).scrollTop() + config.scrollSpeed);
 						}
 					} 
-					//move the cloned element along with the mouse
-					$(".active").css({top: dd.deltaY - halfHandle + scrolled});	
-					
+					//move the cloned element along with the mouse. Not tested across all browsers yet...
+					$('.active').css({top: dd.offsetY - offsetTop});	
+		
 					var drop = dd.drop[0],
-					method = $.data( drop || {}, "drop+reorder" );
-					if ( drop && ( drop != dd.current || method != dd.method ) ){	
+					method = $.data( drop || {}, 'drop+reorder' );
+					if (drop && ( drop != dd.current || method != dd.method)){	
 						$( this )[ method ]( drop );
 						dd.current = drop;
 						dd.method = method;
 						dd.update();
 					}
+				}, { drop: sortScope})
+				
+				.drag('end',function( ev, dd ){		
+					$(this).css('opacity', 1);
+					sortableObject.removeClass('sorting');
+					sortableObject.find('.active').remove();  
+					config.moveComplete();
 				})
 				
-				.drag("end",function( ev, dd ){		
-					$( this ).css("opacity", 1);
-					$(".active").remove(); //scope 
-				})
-				
-				.drop("init",function( ev, dd ){
-					return !( this == dd.drag );
+				.drop('init',function(ev, dd){
+					return !(this == dd.drag);
 				});	
 				
 			$.drop({
 				tolerance: function( event, proxy, target ){
+				
 					var test = event.pageY > ( target.top + target.height / 3.2 );
-					$.data( target.elem, "drop+reorder", test ? "insertAfter" : "insertBefore" );	
+					$.data( target.elem, 'drop+reorder', test ? 'insertAfter' : 'insertBefore' );	
 					return this.contains( target, [ event.pageX, event.pageY ] );
 				}
 			});
 
   };
+
 })( jQuery );
